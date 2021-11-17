c363e4db3763a758e81a848ec0c535b6bb885dd6
Mateusz Kolodziejczyk (20084190) <20084190@IT103iMAC07.wit.ie>
2021-11-17 16:58:13 +0000

    refactors thing

--- Ultimate_X_and_O/main.lua
+++ Ultimate_X_and_O/main.lua
@@ -3,20 +3,18 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
-
 -- Your code here
-
 local mylib = require("mylib")
 local rng = require("rng")
 local colors = require("colorsRGB")
---ai= require("first_space_player")
---ai=require("random_impact_player")
+-- ai= require("first_space_player")
+-- ai=require("random_impact_player")
 ai = require("minimax_player")
 rng.randomseed(os.time())
 
 local backGroup = display.newGroup()
 local mainGroup = display.newGroup()
-local uiGroup = display.newGroup()  
+local uiGroup = display.newGroup()
 
 local board = {} -- 2d repressenation of game board (logic)
 local subboards = {} -- Stores states of the subboards
@@ -24,25 +22,32 @@ local squares = {} -- 1d represetation of game board (ui, events)
 
 -- This allows to draw boards with their centers in relation to the centerx/y values
 local signs = {-1, 0, 1}
-local players = {
-    {name="X", human=true, value=1, wins=0},
-    {name="O", human=true, value=-1, wins=0},
-}
+local players = {{
+    name = "X",
+    human = true,
+    value = 1,
+    wins = 0
+}, {
+    name = "O",
+    human = true,
+    value = -1,
+    wins = 0
+}}
 local player = 1 -- current player
 local gameCount = 0
 local state -- 'waiting', 'thinking', 'over'
 
 local gap = 6
-local size = (math.min(display.contentWidth, display.contentHeight) - 4*gap) / 3
-local subboardScale = 1/3 - ((gap/size)/2)
-local background = display.newImageRect(backGroup,"assets/images/background.png", 444, 794)
+local size = (math.min(display.contentWidth, display.contentHeight) - 4 * gap) / 3
+local subboardScale = 1 / 3 - ((gap / size) / 2)
+local background = display.newImageRect(backGroup, "assets/images/background.png", 444, 794)
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
 local titleText, startsTest, turnText, gameOverText
 local gameOverImage
 
-local forcedSquare, chosenSquare = 0,0
+local forcedSquare, chosenSquare = 0, 0
 -- Functions
 local resetBoard, move, drawBoard
 -----------------------------------------------------------------------------------------
@@ -50,14 +55,20 @@ local resetBoard, move, drawBoard
 -----------------------------------------------------------------------------------------
 local tapSound, winSound, buttonSound
 
-audio.reserveChannels( 3 )
+audio.reserveChannels(3)
 
 -- Reduce the overall volume of the channel
-local bgMusic = audio.loadStream( "assets/audio/bgMusic.mp3" )
+local bgMusic = audio.loadStream("assets/audio/bgMusic.mp3")
 
-audio.setVolume( 0.4, { channel=1 } )
-audio.setVolume( 0.8, { channel=2 } )
-audio.setVolume( 0.9, { channel=3 } )
+audio.setVolume(0.4, {
+    channel = 1
+})
+audio.setVolume(0.8, {
+    channel = 2
+})
+audio.setVolume(0.9, {
+    channel = 3
+})
 -- audio.play( bgMusic, { channel=1, loops=-1 } )
 
 ---------------------------
@@ -66,40 +77,38 @@ audio.setVolume( 0.9, { channel=3 } )
 display.setStatusBar(display.HiddenStatusBar)
 
 -- Line relative to center
-local function drawLine(x1, y1, x2, y2, width, color, centerPos)
+local function drawLine(x1, y1, x2, y2, width, color, centerPos, scale)
     if centerPos == nil then
         centerPos = {}
         centerPos.x = display.contentCenterX
         centerPos.y = display.contentCenterY
     end
-    local line = display.newLine(backGroup,
-        centerPos.x + x1*size, centerPos.y + y1*size,
-        centerPos.x + x2*size, centerPos.y + y2*size
-    )
+    local line = display.newLine(backGroup, centerPos.x + x1 * size * scale, centerPos.y + y1 * size * scale,
+        centerPos.x + x2 * size * scale, centerPos.y + y2 * size * scale)
     color = color or "black"
     line:setStrokeColor(colors.RGB(color))
     width = width or 8
-    line.strokeWidth = width
+    line.strokeWidth = width * scale
 end
 -- display end of game message
-local function displayMessage(message) 
+local function displayMessage(message)
     gameOverText.text = message
     gameOverImage.alpha = 0.5
-    timer.performWithDelay(2500,resetBoard)
+    timer.performWithDelay(2500, resetBoard)
 
 end
 
 drawBoard = function(scale, color, centerPos)
-        -- Vertical lines
-        drawLine(scale*-1/2, scale*-3/2, scale*-1/2, scale*3/2, scale*8, color, centerPos)
-        drawLine(scale*1/2, scale*-3/2, scale*1/2, scale*3/2, scale*8, color, centerPos)
-        -- Horizontal lines
-        drawLine(scale*-3/2, scale*-1/2, scale*3/2, scale*-1/2, scale*8, color, centerPos)
-        drawLine(scale*-3/2, scale*1/2,scale*3/2, scale*1/2, scale*8, color, centerPos)
+    -- Vertical lines
+    drawLine(-1 / 2, -3 / 2, -1 / 2, 3 / 2, 8, color, centerPos, scale)
+    drawLine(1 / 2, -3 / 2, 1 / 2, 3 / 2, 8, color, centerPos, scale)
+    -- Horizontal lines
+    drawLine(-3 / 2, -1 / 2, 3 / 2, -1 / 2, 8, color, centerPos, scale)
+    drawLine(-3 / 2, 1 / 2, 3 / 2, 1 / 2, 8, color, centerPos, scale)
 end
 -- Switch to next player or given player index
 local function nextPlayer(value)
-    player = value or (player%2 + 1)
+    player = value or (player % 2 + 1)
 
     state = players[player].human and "waiting" or "thinking"
 
@@ -117,13 +126,13 @@ move = function(k)
 
     -- update ui and logic
     local filename = "assets/images/" .. players[player].name .. ".png"
-    local symbol = display.newImageRect(mainGroup, filename, size-4*gap, size-4*gap)
+    local symbol = display.newImageRect(mainGroup, filename, size - 4 * gap, size - 4 * gap)
     symbol.x = square.rect.x
     symbol.y = square.rect.y
     square.symbol = symbol
     board[k] = players[player].value
     -- check if game win
-    if mylib.isWin(board) then 
+    if mylib.isWin(board) then
         state = 'over'
         gameCount = gameCount + 1
         players[player].wins = players[player].wins + 1
@@ -145,7 +154,7 @@ local function checkMove(event)
     -- determine location of tap on board
     print(players[player].name .. "'s move at square " .. event.target.k)
     -- current player must be human
-    if state~="waiting" then
+    if state ~= "waiting" then
         print("\t not waiting for human input - ignore move")
         return false
     end
@@ -159,7 +168,7 @@ local function checkMove(event)
 end
 
 -- reset game state (without unnecessary destroying)
-resetBoard = function ()
+resetBoard = function()
 
     -- tidy up of UI elements
     for _, square in ipairs(squares) do
@@ -167,58 +176,43 @@ resetBoard = function ()
         square.symbol = nil
     end
     local tieCount = gameCount - players[1].wins - players[2].wins
-    local s = string.format("Games: %3d    %s: %d    %s: %d    tie: %d",
-        gameCount, 
-        players[1].name, players[1].wins, 
-        players[2].name, players[2].wins, 
-        tieCount
-    )
+    local s = string.format("Games: %3d    %s: %d    %s: %d    tie: %d", gameCount, players[1].name, players[1].wins,
+        players[2].name, players[2].wins, tieCount)
     statsText.text = s
     gameOverImage.alpha = 0
     gameOverText.text = ""
     -- reset game logic
     board = {}
     for k = 1, 9 do
-            board[k] = 0
+        board[k] = 0
     end
     nextPlayer(1)
 end
--- Populates boards with clickable squares
-local function populateBoard(scale, centerPos, color, alpha)
-    drawBoard(scale, color, centerPos)
-    for k = 1, 9 do
-        local row, col = mylib.k2rc(k)
-        local x = centerPos.x + (col-4/2)*size*subboardScale
-        local y = centerPos.y + (row-4/2)*size*subboardScale
-        local rect = display.newRect(uiGroup, x, y, (size-gap)*scale, (size-gap)*scale)
-        rect.alpha = alpha or 0.05
-        rect.k = k
-        rect:addEventListener("tap", checkMove )
-        squares[k] = {rect=rect}
-    end
-end
+
 local function createBoard()
     for r = 1, 3 do
         for c = 1, 3 do
             -- Draw the subboards
-            populateBoard(subboardScale,
-            {   
+            drawBoard(subboardScale, "slategray",
+            {
                 x = display.contentCenterX + size * signs[r],
                 y = display.contentCenterY + size * signs[c]
-            },  "slategrey", 0.5)
-        end 
+            })
+        end
     end
-    populateBoard(1, {x = display.contentCenterX, y = display.contentCenterY})
+    drawBoard(1)
 
     for k = 1, 9 do
         local row, col = mylib.k2rc(k)
-        local x = display.contentCenterX + (col-4/2)*size
-        local y = display.contentCenterY + (row-4/2)*size
-        local rect = display.newRect(uiGroup, x, y, size-gap, size-gap)
+        local x = display.contentCenterX + (col - 4 / 2) * size
+        local y = display.contentCenterY + (row - 4 / 2) * size
+        local rect = display.newRect(uiGroup, x, y, size - gap, size - gap)
         rect.alpha = 0.05
         rect.k = k
-        rect:addEventListener("tap", checkMove )
-        squares[k] = {rect=rect}
+        rect:addEventListener("tap", checkMove)
+        squares[k] = {
+            rect = rect
+        }
     end
     -- Transparent overlay
     gameOverImage = display.newRect(mainGroup, 0, 0, display.actualContentWidth, display.actualContentHeight)
@@ -230,12 +224,12 @@ local function createBoard()
     -- Instantiating text
     titleText = display.newText(uiGroup, "X's and O's", 100, 200, "assets/fonts/Bangers.ttf", 40)
     titleText.x = display.contentCenterX
-    titleText.y = display.contentCenterY - 2.5*size
+    titleText.y = display.contentCenterY - 2.5 * size
     titleText:setFillColor(colors.RGB("moccasin"))
 
     statsText = display.newText(uiGroup, "", 100, 200, "assets/fonts/Bangers.ttf", 26)
     statsText.x = display.contentCenterX
-    statsText.y = display.contentCenterY - 2*size
+    statsText.y = display.contentCenterY - 2 * size
     -- Initiate game over text
     gameOverText = display.newText(uiGroup, "", 100, 200, "assets/fonts/Bangers.ttf", 40)
     gameOverText.x = display.contentCenterX
